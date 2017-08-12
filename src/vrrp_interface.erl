eb91bd822b2706583d710a488b185c6e0b305d42
Rick Payne <rickp@OtoloNetworks.com>
2017-08-11 22:31:43 -0500

    Start of work to receive and send packets. Initially we use vrrp_socket to create the RAW socket bound to the right interface (this means we need to be root for now). Use gen_udp to recvfrom() the messages and pass them to the vrrp_interface process

--- src/vrrp_interface.erl
+++ src/vrrp_interface.erl
@@ -10,6 +10,8 @@
 
 -behaviour(gen_server).
 
+-include("vrrp_protocol.hrl").
+
 %% API
 -export([start_link/1]).
 
@@ -23,7 +25,8 @@
 
 -record(state, {
           interface,   %% Physical interface we are running on
-          port,
+          v4_rsocket,
+          wsocket,
           mapping      %% Dict of Id -> FSM Pid
          }).
 
@@ -64,12 +67,20 @@ remove_mapping(Interface, Id) ->
 init(Args) ->
     process_flag(trap_exit, true),
     Interface = proplists:get_value(interface, Args),
-    {ok, S} = procket:open(0, [{protocol, 16#112}, {type, raw}, {family, packet},
-                               {interface, Interface}]),
-    Port = erlang:open_port({fd, S, S}, [binary, stream]),
+
+    %% This is a bit ugly! We use a NIF to create the sockets and then
+    %% we trick gen_udp into giving us the data, including the source
+    %% address (i.e. recvfrom()).
+    {R, _W} = vrrp_socket:create_socket(ipv4, Interface),
+    {ok, Socket} = gen_udp:open(0, [{fd, R}, binary]),
+    ok = gen_udp:controlling_process(Socket, self()),
+
+    %%% Rinse and repeat for the v6 and writer sockets...
+
+    %% Done
     {ok, #state{
             interface = Interface,
-            port = Port,
+            v4_rsocket = Socket,
             mapping = dict:new()}}.
 
 %%--------------------------------------------------------------------
@@ -142,6 +153,8 @@ handle_info({'EXIT', Pid, shutdown}, #state{mapping = D} = State) ->
     %% One of our FSM's has shutdown normally...
     ND = dict:filter(fun(_K, {VP, _VR}) -> VP =/= Pid end, D),
     {noreply, State#state{mapping = ND}};
+handle_info({udp, _Socket, Source, _Zero, Pkt}, State) ->
+    {noreply, handle_packet(verify_packet(Source, Pkt, State))};
 handle_info(Info, State) ->
     io:format("vrrp_interface received: ~p~n", [Info]),
     {noreply, State}.
@@ -174,3 +187,71 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+%%%===================================================================
+%%% Verify Packet
+%%% Pattern match to parse the IP part of the packet and if thats ok,
+%%% then go onto verify the VRRP part...
+%%% TTL must be 255, Protocol must be 112.
+%%%===================================================================
+verify_packet(Source,
+              <<4:4/integer, 5:4/integer, _:8, Len:16/big-integer,
+                _:16, _:2, _:1, 0:13, 255:8, 112:8/integer,
+                _:16, Src_Ip:32/big-integer, Dst_Ip:32/big-integer,
+                Remainder/binary>> = Pkt,
+              State)
+  when byte_size(Pkt) =:= Len ->
+    %% Initial parse...
+    
+    %% <<4:4/integer,          % Ip Version
+    %%  5:4/integer,      % Header Length
+    %%  _:8/integer,           % Tos -- Ignored
+    %%  Len:16/big-integer,    % Packet Length
+    %%  _:16/big-integer, % Fragment Id
+    %%  _:2/integer,           % Two first flags... ignored
+    %%  Mf:1/integer,          % More Fragments flag
+    %%  Offset:13/big-integer, % Fragment Offset
+    %%  255:8/integer,           % TTL -- We are not routing(yet) so we don't care
+    %%  112:8/integer,    % Upper layer protocol
+    %%  _:16/big-integer,      % Checksum... Alredy checked
+    %%  Src_Ip:32/big-integer, % Source Ip
+    %%  Dst_Ip:32/big-integer, % Should check this against our Ip
+    %%  Remainder/binary>> = Packet, % We don't do options yet 
+    verify_vrrp(Source, Remainder, State);
+verify_packet(_Source, _Packet, State) ->
+    io:format("Z~n", []),
+    {bad_packet, State}.
+
+verify_vrrp(Source,
+            <<3:4, 1:4, ID:8/integer, Priority:8/integer, Count:8/integer, 
+              0:4, Interval:12/big-integer, _CSum:16/big-integer,
+              BinIPs/binary>>,
+            State)
+ when Count == (byte_size(BinIPs)/4)->
+    %% TODO - Check checksum..
+
+    %% Map IPs into something useful...
+    IPs = [ {A, B, C, D} || <<A:8, B:8, C:8, D:8>> <= BinIPs ],
+
+    {#vrrp_packet{
+        from = Source,
+        version = 3,
+        type = announce,
+        id = ID,
+        priority = Priority,
+        interval = Interval,
+        ips = IPs
+       }, State};
+verify_vrrp(_Source, _Packet, State) ->
+    {bad_packet, State}.
+
+handle_packet({#vrrp_packet{id = Id} = Pkt, #state{mapping = D} = State}) ->
+    case dict:find(Id, D) of
+        {ok, {Pid, _MR}} ->
+            vrrp_fsm:vrrp_msg(Pid, Pkt);
+        error ->
+            discard
+    end,
+    State;
+handle_packet({bad_packet, State}) ->
+    State.
