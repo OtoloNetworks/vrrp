1d9a9ec786a594a1e9c559f1b8571561c21ff299
Rick Payne <rickp@OtoloNetworks.com>
2017-09-23 14:58:42 +1300

    Add some startup facility

--- src/vrrp_app.erl
+++ src/vrrp_app.erl
@@ -15,7 +15,9 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
-    vrrp_sup:start_link().
+    R = vrrp_sup:start_link(),
+    start_all(application:get_env(vrrp, instance)),
+    R.
 
 %%--------------------------------------------------------------------
 stop(_State) ->
@@ -24,3 +26,11 @@ stop(_State) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+start_all(undefined) ->
+    ok;
+start_all({ok, Keys}) ->
+    lists:foreach(
+      fun({Id, Params}) ->
+              vrrp_sup:start_vrrp([{id, Id}] ++ Params)
+      end, Keys).
+
